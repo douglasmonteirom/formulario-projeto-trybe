543139938b1ef830674ddef672c5da7591dd6b89
douglas <douglasmonteiro.m@gmail.com>
2021-10-21 22:51:02 -0300

    Adiciona requisito 5

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,12 +1,49 @@
 import React from 'react';
 import Header from '../components/Header';
 
+const MIN_CHARACTERS_ARTIST = 2;
+
 class Search extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      buttonDisable: true,
+    };
+  }
+
+  handleState = ({ target }) => {
+    if (target.value.length >= MIN_CHARACTERS_ARTIST) {
+      this.setState({
+        buttonDisable: false,
+      });
+    } else {
+      this.setState({
+        buttonDisable: true,
+      });
+    }
+  }
+
   render() {
+    const { buttonDisable } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
-        <p>Search</p>
+        <label htmlFor="true">
+          <input
+            data-testid="search-artist-input"
+            type="text"
+            placeholder="Nome do Artista"
+            onChange={ (event) => this.handleState(event) }
+
+          />
+        </label>
+        <button
+          data-testid="search-artist-button"
+          type="button"
+          disabled={ buttonDisable }
+        >
+          Pesquisar
+        </button>
       </div>
     );
   }
